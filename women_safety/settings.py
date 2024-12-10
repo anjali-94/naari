458757bd801113ac0732709454d4c25f718fe7f0
anjali-94 <anjalishr987@gmail.com>
2024-12-10 08:28:00 +0530

    second commit

--- women_safety/settings.py
+++ women_safety/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'women_safety',
 ]
 
 #to use backend authentication of django
